96f7bcedc0db5b12bbb41998a164a790f3d3e3df
Yasmim Jorge <yasmim.jorge@global.ntt>
2026-01-21 22:32:06 -0300

    Atualizando routes

--- auth_routes.py
+++ auth_routes.py
@@ -1,9 +1,26 @@
 from fastapi import APIRouter
+from models import Usuario
+from sqlalchemy.orm import sessionmaker
+
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def autenticar():
+async def home():
     """
     Essa é a rota de autenticação do sistema
     """
     return {"mensagem": "Vcoê acessou a rota padrão de autenticação", "autenticado":False}
+
+@auth_router.post("/criar")
+async def criar_conta(email: str, senha: str):
+    Session = sessionmaker(bind=db)
+    session = Session()
+    usuario = session.query(Usuario).filter(usuario.email == email).first()
+    if usuario:
+        #Já existe um usuário com esse e-mail
+        return {"mensagem": "Já existe um usuário com esse e-mail"}
+    else: 
+        novo_usuario = Usuario(nome: str, email:str, senha:str)
+        session.add(novo_usuario)
+        session.commit()
+        return {"Mensagem": "Usuario cadastrado com sucesso!"}
